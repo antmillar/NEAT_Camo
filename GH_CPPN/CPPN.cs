d9e4753ab968ced5313e62a2f3c4b6f0d531caf9
HarryGateaux <antmillar@gmail.com>
2020-04-05 16:19:56 +0100

    Add project files.

---
+++ GH_CPPN/CPPN.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Drawing;
+using NumSharp;
+
+namespace GH_CPPN
+{
+    public class CPPN : GH_Component
+    {
+
+        public CPPN() : base("CPPN", "CPPN", "Constructing a 2d CPPN", "CPPN", "Simple")
+            {
+
+            }
+
+        public override Guid ComponentGuid
+        {
+            // Don't copy this GUID, make a new one
+            get { return new Guid("ab047315-77a4-45ef-8039-44cd6428b913"); }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            //pManager.AddTextParameter("input text", "i", "string to reverse", GH_ParamAccess.item);
+            //pManager.AddNumberParameter("input number", "num", "number to input", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            //pManager.AddPointParameter("points", "pts", "grid points", GH_ParamAccess.list);
+            pManager.AddMeshParameter("mesh grid", "mg", "grid of meshes", GH_ParamAccess.item);
+            pManager.AddNumberParameter("outputs", "outputs", "output of linear", GH_ParamAccess.list);
+
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            //double data = double.NaN;
+
+            //if (!DA.GetData(0, ref data)) { return; }
+
+            //if (data == double.NaN) { return; }
+
+
+
+            System.Random rand = new System.Random();
+
+            List<Mesh> meshes = new List<Mesh>();
+            int width = 100;
+
+            NDArray coords = np.ones((width * width, 2));
+
+            for (int i = -width/2; i < width/2; i++)
+            {
+                for (int j = -width/2; j < width/2; j++)
+                {
+                    var cell = new Mesh();
+
+                    cell.Vertices.Add(i, j, 0.0);
+                    cell.Vertices.Add(i + 1, j, 0.0);
+                    cell.Vertices.Add(i, j + 1, 0.0);
+                    cell.Vertices.Add(i + 1, j + 1, 0.0);
+
+                    cell.Faces.AddFace(0, 1, 3, 2);
+
+                    meshes.Add(cell);
+
+                    //coords are in range [-0.5, 0.5]
+                    coords[(i + width/2)* width + j + width/2, 0] = 1.0 * i / width;
+                    coords[(i + width/2)* width + j + width/2, 1] = 1.0 * j / width;
+
+                }
+            }
+
+            Rhino.Geometry.Plane plane = Rhino.Geometry.Plane.WorldXY;
+
+            var linear = new CustomModel();
+            //NDArray activations = linear.ForwardPass(coords);
+
+            var mlp = new MLP(2, 1, 8);
+            NDArray activations = mlp.ForwardPass(coords);
+
+            for (int i = 0; i < meshes.Count; i++)
+       
+            {
+                double col = activations[i, 0];
+                int intCol = (int) (col * 255);
+                Color color = Color.FromArgb(intCol, intCol, intCol);
+
+                Color[] colors = Enumerable.Repeat(color, 4).ToArray();
+                meshes[i].VertexColors.AppendColors(colors);
+            }
+
+            Mesh combinedMesh = new Mesh();
+            combinedMesh.Append(meshes);
+            DA.SetData(0, combinedMesh);
+            DA.SetDataList(1, activations);
+        }
+    }
+}
---
+++ GH_CPPN/Linear.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumSharp;
+
+namespace GH_CPPN
+{
+  
+    //MODELS
+    public interface Model
+    {
+        NDArray ForwardPass(NDArray input);
+    }
+
+    public class CustomModel : Model
+    {
+        public NDArray ForwardPass(NDArray input)
+        {
+            //input has size numDims x inputFeats
+            //output has size numDims x outputFeats
+            //weights has size inputFeats x outputFeats
+
+            //TODO create weights, create biases,
+
+            //number of input feats
+            int num_input = input.shape[1];
+            int num_hidden = 16;
+            int num_output = 1;
+
+            var sigmoid = new Sigmoid();
+            var tanh = new Tanh();
+
+            var fc1 = new Linear(num_input, num_hidden);
+            var x = fc1.Apply(input);
+            x = tanh.Apply(x);
+
+            var fc2 = new Linear(num_hidden, num_hidden);
+            x = fc2.Apply(x);
+            x = tanh.Apply(x);
+
+            var fc3 = new Linear(num_hidden, num_output);
+            x = fc3.Apply(x);
+            x = sigmoid.Apply(x);
+
+            return x;
+        }
+    }
+
+    public class MLP : Model
+    {
+        /// <summary> Creates a model with a num_layers each with num_neurons. </summary>
+
+        List<ILayer> layers;
+
+        public MLP(int _input_feats, int _output_feats, int _num_hiddenlayers = 0, int _num_neurons = 16)
+        {
+            layers = new List<ILayer>();
+            var layerIn = new Linear(_input_feats, _num_neurons);
+            layers.Add(layerIn);
+
+            //hidden layers
+            for (int i = 0; i < _num_hiddenlayers; i++)
+            {
+                layers.Add(new Linear(_num_neurons, _num_neurons));
+            }
+
+            var layerOut = new Linear(_num_neurons, _output_feats);
+            layers.Add(layerOut);
+        }
+
+        public NDArray ForwardPass(NDArray input)
+        {
+            var x = input;
+            var tanh = new Tanh();
+            var sigmoid = new Sigmoid();
+
+            for (int i = 0; i < layers.Count - 1; i++)
+            {
+                x = layers[i].Apply(x);
+                x = tanh.Apply(x);
+            }
+
+            //final layer needs to be sigmoid so output in range (0, 1)
+            x = layers[layers.Count - 1].Apply(x);
+            x = sigmoid.Apply(x);
+
+            return x;
+        }
+    }
+
+
+    //LAYERS
+    public interface ILayer
+    {
+        NDArray Apply(NDArray input);
+
+    }
+
+    public class Linear : ILayer
+    {
+        NDArray weights;
+        NDArray biases;
+
+        public Linear(int _input_feats, int _output_feats, bool _bias = true)
+        {
+            //initialise weights and biases
+            weights = np.random.uniform(-1.0, 1.0, (_input_feats, _output_feats));
+            biases = np.random.uniform(-1.0, 1.0, (1, _output_feats));// : np.zeros((1 , _output_feats)); //need to initialise these at some point
+  
+        }
+        public NDArray Apply(NDArray input)
+        {
+ 
+            return np.matmul(input, weights) + biases;
+        }
+    }
+
+    //ACTIVATIONS
+    //interface for activation functions
+    public interface IActivation
+    {
+        NDArray Apply (NDArray input);
+    }
+
+    public class Sigmoid : IActivation
+    {
+        public NDArray Apply(NDArray input)
+        {
+            return 1.0 / (1.0 + np.exp(-1.0 * input));
+        }
+    }
+
+    public class Tanh : IActivation
+    {
+        public NDArray Apply(NDArray input)
+        {
+            return (np.exp(input) - np.exp(-1.0 * input)) / (np.exp(input) + np.exp(-1.0 * input));
+        }
+    }
+
+    public class Relu : IActivation
+    {
+        public NDArray Apply(NDArray input)
+        {
+            return np.maximum(input, 0);
+        }
+
+
+    }
+
+}
