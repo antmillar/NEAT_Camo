9a45afd6a038fded281228e6bd5952630bdf8535
HarryGateaux <antmillar@gmail.com>
2020-04-06 10:16:37 +0100

    misc

--- GH_CPPN/NEAT.cs
+++ GH_CPPN/NEAT.cs
@@ -4,29 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GH_CPPN
+namespace WIP
 {
-    class NEAT
+    public class NEAT
     {
         public static System.Random globalRandom = new System.Random();
+        public static double mutateRate = 0.2;
 
         public class Population
         {
-            public List<Genome> _genomes;
+            List<Genome> Genomes { get; set; }
 
             public Population(int size)
             {
-                _genomes = new List<Genome>();
+                Genomes = new List<Genome>();
 
                 for (int i = 0; i < size; i++)
                 {
-                    _genomes.Add(new Genome());
+                    Genomes.Add(new Genome());
                 }
+            }
+
+
+            public void NextGeneration()
+            {
+                foreach(var genome in Genomes)
+                {
+                    //could create a copy in here instead and return it
+                    genome.Mutate();
+                }
+
             }
 
             public override string ToString()
             {
-                return String.Join("\n", _genomes.Select(x => x.ToString()));
+                return String.Join("\n", Genomes.Select(x => x.ToString()));
             }
 
         }
@@ -37,11 +49,15 @@ namespace GH_CPPN
         {
             string _activationType;
             int _id;
+            string _type;
 
-            public NodeGene(int id)
+            public NodeGene(int id, string type)
             {
                 _id = id;
+                _type = type;
             }
+
+
 
             public override string ToString()
             {
@@ -51,7 +67,7 @@ namespace GH_CPPN
         }
         public class ConnectionGene : Gene
         {
-            double _weight;
+            public double Weight { get; set; }
             int _inputNode;
             int _outputNode;
 
@@ -60,44 +76,67 @@ namespace GH_CPPN
                 _inputNode = inputNode;
                 _outputNode = outputNode;
 
-                _weight = globalRandom.NextDouble() * 2 - 1.0;
+                Weight = globalRandom.NextDouble() * 2 - 1.0;
             }
 
             public override string ToString()
             {
-                return $"({_inputNode}, {_outputNode}) w = {Math.Round(_weight, 2)} ";
+                return $"({_inputNode}, {_outputNode}) w = {Math.Round(Weight, 2)} ";
             }
         }
 
 
         public class Genome
         {
-            List<Gene> _nodes;
-            List<Gene> _connections;
+            List<Gene> Nodes { get; set; }
+            List<Gene> Connections { get; set; }
+
             float _fitness;
 
             public Genome()
             {
                 //initialise a standard architecture
-                _nodes = new List<Gene>();
-                _connections = new List<Gene>();
+                Nodes = new List<Gene>();
+                Connections = new List<Gene>();
 
-                _nodes.Add(new NodeGene(0));
-                _nodes.Add(new NodeGene(1));
-                _nodes.Add(new NodeGene(2));
-                _nodes.Add(new NodeGene(3));
+                Nodes.Add(new NodeGene(0, "input"));
+                Nodes.Add(new NodeGene(1, "input"));
+                Nodes.Add(new NodeGene(2, "hidden"));
+                Nodes.Add(new NodeGene(3, "hidden"));
+                Nodes.Add(new NodeGene(4, "output"));
 
-                _connections.Add(new ConnectionGene(0, 1));
-                _connections.Add(new ConnectionGene(0, 2));
-                _connections.Add(new ConnectionGene(1, 3));
-                _connections.Add(new ConnectionGene(2, 3));
+                Connections.Add(new ConnectionGene(0, 2));
+                Connections.Add(new ConnectionGene(0, 3));
+                Connections.Add(new ConnectionGene(1, 2));
+                Connections.Add(new ConnectionGene(1, 3));
+                Connections.Add(new ConnectionGene(2, 4));
+                Connections.Add(new ConnectionGene(3, 4));
+
+            }
+
+            //mutates just the connection genes currently
+            public void Mutate()
+            {
+                foreach(ConnectionGene connection in Connections)
+                {
+                    if (globalRandom.NextDouble() < mutateRate)
+                    {
+                        //need to clamp weights?
+                        connection.Weight += (globalRandom.NextDouble() - 0.5);
+                    }
+                }
+            }
+
+            public void CrossOver(Genome other)
+            {
+
 
             }
 
             public override string ToString()
             {
-                string s = "nodes : " + String.Join(" ", _nodes.Select(x => x.ToString()));
-                s += " conns : " + String.Join(" ", _connections.Select(x => x.ToString()));
+                string s = "Nodes : " + String.Join(" ", Nodes.Select(x => x.ToString()));
+                s += " | Conns : " + String.Join(" ", Connections.Select(x => x.ToString()));
 
                 return s;
             }
